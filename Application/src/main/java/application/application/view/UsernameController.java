d8085ced67f192d945e9a51f25ccb1f2422ee052
totlevi <tothlevente4321@gmail.com>
2023-03-21 11:15:44 +0100

    window
    loaders to connect windows

--- Application/src/main/java/application/application/view/UsernameController.java
+++ Application/src/main/java/application/application/view/UsernameController.java
@@ -1,5 +1,29 @@
 package application.application.view;
 
+import application.application.viewmodel.UsernameViewModel;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.Region;
+
 public class UsernameController
 {
+
+  private UsernameViewModel unmodel;
+  private ViewHandler vhandler;
+
+  @FXML private TextField username;
+  @FXML private Button createUser;
+  @FXML private Button viewChatAsGuest;
+
+  private Region root;
+
+  public void init(ViewHandler viewHandler, UsernameViewModel usernameViewModel, Region root){
+
+    this.unmodel = usernameViewModel;
+  }
+
+
+
+
 }
--- Application/src/main/java/application/application/view/ViewFactory.java
+++ Application/src/main/java/application/application/view/ViewFactory.java
@@ -1,5 +1,72 @@
 package application.application.view;
 
+import application.application.viewmodel.ChatViewModel;
+import application.application.viewmodel.UsernameViewModel;
+import application.application.viewmodel.ViewModelFactory;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.Region;
+
+import java.io.IOError;
+import java.io.IOException;
+
 public class ViewFactory
 {
+  private ViewHandler vhandler;
+  private ViewModelFactory vmfactory;
+  private ChatController cvmodel;
+  private UsernameController unmodel;
+
+  public ViewFactory(ViewHandler viewHandler, ViewModelFactory viewModelFactory){
+    this.vmfactory = viewModelFactory;
+    this.vhandler = viewHandler;
+    cvmodel = null;
+    unmodel = null;
+  }
+
+  public Region loadView(String id){
+    return switch (id){
+      case"usernameView" -> loadUsernameView();
+      case"chatView" -> loadChatView();
+      default -> throw new IllegalArgumentException("Unknown view: " +id);
+    };
+  }
+
+  public Region loadUsernameView(){
+
+    if (unmodel == null) {
+      FXMLLoader loader = new FXMLLoader();
+      loader.setLocation(getClass().getResource("application/application/username.fxml"));
+      try {
+        Region root = loader.load();
+        unmodel = loader.getController();
+        unmodel.init(vhandler, vmfactory.getUsernameViewModel(), root);
+      } catch (IOException e) {
+        throw new IOError(e);
+      }
+    }
+
+    unmodel.reset();
+    return unmodel.getRoot();
+  }
+
+  public Region loadChatView(){
+
+    if (cvmodel == null) {
+      FXMLLoader loader = new FXMLLoader();
+      loader.setLocation(getClass().getResource("application/application/chat.fxml"));
+      try {
+        Region root = loader.load();
+        cvmodel = loader.getController();
+        cvmodel.init(vhandler, vmfactory.getChatViewModel(), root);
+      } catch (IOException e) {
+        throw new IOError(e);
+      }
+    }
+
+    cvmodel.reset();
+    return cvmodel.getRoot();
+  }
+
+}
+
 }
