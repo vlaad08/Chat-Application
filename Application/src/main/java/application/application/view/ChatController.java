5193c56566699bbe31badc389a89813ad9e3c2eb
vlaad08 <62983956+vlaad08@users.noreply.github.com>
2023-03-21 11:32:34 +0100

    a

--- Application/src/main/java/application/application/view/ChatController.java
+++ Application/src/main/java/application/application/view/ChatController.java
@@ -1,5 +1,38 @@
 package application.application.view;
 
+import application.application.viewmodel.ChatViewModel;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.Region;
+
 public class ChatController
 {
+  @FXML private TextArea chatTextArea;
+  @FXML private TextField inputTextField;
+  @FXML private Button sendButton;
+
+  private Region root;
+  private ViewHandler viewHandler;
+  private ChatViewModel viewModel;
+
+  public void init(ViewHandler viewHandler, ChatViewModel chatViewModel, Region root)
+  {
+    this.viewHandler = viewHandler;
+    this.viewModel = chatViewModel;
+    this.root = root;
+  }
+
+  public Region getRoot()
+  {
+    return root;
+  }
+
+  public void  reset()
+  {
+    inputTextField.setText("");
+  }
+
+
 }
--- Application/src/main/java/application/application/viewmodel/ChatViewModel.java
+++ Application/src/main/java/application/application/viewmodel/ChatViewModel.java
@@ -1,5 +1,17 @@
 package application.application.viewmodel;
 
+import application.application.model.Model;
+import application.application.model.ModelManager;
+import javafx.beans.property.ObjectProperty;
+
 public class ChatViewModel
 {
+  private Model model;
+
+  private ObjectProperty<P>
+
+  public ChatViewModel(Model model)
+  {
+    this.model = model;
+  }
 }
