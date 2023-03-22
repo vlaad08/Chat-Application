2f529fe3d1099e834c43c2821d79b900e2609fd4
vlaad08 <62983956+vlaad08@users.noreply.github.com>
2023-03-22 14:14:23 +0100

    Revert "q"
    This reverts commit a2eb5a0704094a5464c70865143436a658469a9f.

--- Application/src/main/java/chat/server/LoginCommunicator.java
+++ Application/src/main/java/chat/server/LoginCommunicator.java
@@ -1,8 +1,5 @@
 package chat.server;
 
-import chat.model.Model;
-import chat.model.ModelManager;
-
 import java.io.*;
 import java.net.Socket;
 
@@ -11,7 +8,7 @@ public class LoginCommunicator implements Runnable
 {
   private final UDPBroadcaster broadcaster;
   private final Socket socket;
-
+  //private final Gson gson;
 
   public LoginCommunicator(Socket socket, UDPBroadcaster broadcaster){
     this.socket = socket;
@@ -35,8 +32,6 @@ public class LoginCommunicator implements Runnable
           writer.println("connected");
           System.out.println("connected");
           writer.flush();
-
-
 
           while (true)
           {
